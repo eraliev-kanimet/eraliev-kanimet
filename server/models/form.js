97e64c34d5fc0c2e54a0d0ca6002a49da519d8f1
Kanimet <kanimet200127@gmail.com>
2024-07-30 05:57:42 +0600

    Created the table “forms” and model (Form) on the server

---
+++ server/models/form.js
@@ -0,0 +1,27 @@
+import {DataTypes} from "sequelize";
+
+import sequelize from "./sequelize.js"
+
+const Form = sequelize.define("form", {
+    type: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    data: {
+        type: DataTypes.JSON,
+        allowNull: false,
+    }
+}, {
+    updatedAt: "updated_at",
+    createdAt: "created_at",
+});
+
+(async () => {
+    await sequelize.sync();
+})();
+
+export default Form
