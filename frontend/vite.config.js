71cc87cfaa93e346b2b4ced6cfe77e174b76e094
Kanimet <kanimet200127@gmail.com>
2024-07-30 14:39:28 +0600

    Changed the files for the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'admin/index.html'),
             },
         },
     },
