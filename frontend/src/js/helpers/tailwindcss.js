bb7d0049608288eaec2315bb2c6e1352d5987d1e
Kanimet <kanimet200127@gmail.com>
2024-07-30 14:53:42 +0600

    Created a helper function to work with tailwindcss

---
+++ frontend/src/js/helpers/tailwindcss.js
@@ -0,0 +1,28 @@
+
+export function tailwindcssApplyTheme(theme) {
+    const root = document.documentElement;
+
+    if (theme === 'dark') {
+        root.classList.add('dark');
+        root.classList.remove('light');
+
+        return 'dark'
+    } else if (theme === 'light') {
+        root.classList.add('light');
+        root.classList.remove('dark');
+
+        return 'light'
+    } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            root.classList.add('dark');
+            root.classList.remove('light');
+
+            return 'dark'
+        } else {
+            root.classList.add('light');
+            root.classList.remove('dark');
+
+            return 'light'
+        }
+    }
+}
