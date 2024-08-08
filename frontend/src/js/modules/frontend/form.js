6327da96a56f736d5bb0e8069d8a1fffdbcd6384
Kanimet <kanimet200127@gmail.com>
2024-08-09 00:42:23 +0600

    Made the logic of working with the "Sending Message" form

--- frontend/src/js/modules/frontend/form.js
+++ frontend/src/js/modules/frontend/form.js
@@ -1,3 +1,5 @@
+import formService from "../../services/formService.js";
+
 import {closeModals, openModals} from "../../helpers/modal.js";
 
 const formModal = (form) => {
@@ -13,4 +15,22 @@ export default function () {
     const form = document.getElementById('form');
 
     formModal(form);
+
+    const service = formService(form, {
+        name: '',
+        category: '',
+        telegram: '',
+        message: '',
+    }, {
+        name: ['required', 'string'],
+        category: ['required', 'string'],
+        telegram: ['required', 'string'],
+        message: ['required', 'max:255'],
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        service.submit();
+    });
 }
