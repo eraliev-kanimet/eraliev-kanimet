7f23a05a5c174593b364e44768f517405c7b5a55
Kanimet <kanimet200127@gmail.com>
2024-08-10 13:00:06 +0600

    Added "select-v2" component to initializeForm.js

--- frontend/src/js/helpers/form/initializeForm.js
+++ frontend/src/js/helpers/form/initializeForm.js
@@ -1,14 +1,29 @@
 import {validate} from "./validate.js";
 import validateAll from "./validate.js";
 import showError from "./showError.js";
+import selectV2 from "../../components/select-v2.js";
 
 export default function (form, schema) {
     const data = {};
     const rules = {};
     const formErrors = {};
 
+    if (form.tagName !== 'FORM') {
+        throw new Error(`Fatal error: This service is only used for the html tag "form"`);
+    }
+
+    const btn = form.querySelector('button[type="submit"]');
+
+    if (!btn) {
+        throw new Error(`Fatal error: Item not found (button[type="submit"])`);
+    }
+
     for (const key in schema) {
-        data[key] = form.querySelector(`[name="${key}"]`);
+        if (schema[key].tag === 'select-v2') {
+            data[key] = form.querySelector(`[data-select="${key}"]`);
+        } else {
+            data[key] = form.querySelector(`[name="${key}"]`);
+        }
 
         if (!data[key]) {
             throw new Error(`Fatal error: Element with attribute name="${key}" not found!`);
@@ -19,17 +34,25 @@ export default function (form, schema) {
         if (error) {
             formErrors[key] = error;
 
-            if (schema[key]?.rules && schema[key].rules.length) {
-                rules[key] = schema[key].rules;
+            if (schema[key].tag !== 'select-v2') {
+                if (schema[key]?.rules && schema[key].rules.length) {
+                    rules[key] = schema[key].rules;
 
-                if (schema[key].mode === 'input') {
-                    data[key].addEventListener('input', (e) => {
-                        showError(e.target, error, validate(e.target.value, schema[key].rules));
-                    })
+                    if (schema[key].mode === 'live') {
+                        const event = data[key].tagName === 'SELECT' ? 'change' : 'input';
+
+                        data[key].addEventListener(event, (e) => {
+                            showError(e.target, error, validate(e.target.value, schema[key].rules));
+                        })
+                    }
                 }
             }
         } else {
             console.warn(`Warning: Element with attribute data-error="${key}" not found!`);
+        }
+
+        if (schema[key].tag === 'select-v2') {
+            selectV2(data[key], error, {mode: schema[key].mode, rules: schema[key].rules, ...schema[key].options})
         }
     }
 
@@ -40,7 +63,7 @@ export default function (form, schema) {
             showError(data[key], formErrors[key], errors[key]);
         }
 
-        return Object.values(errors).filter(item => !item.length).length;
+        return !Boolean(Object.values(errors).filter(item => item.length).length);
     }
 
     function reset() {
@@ -56,6 +79,7 @@ export default function (form, schema) {
 
     return {
         data,
+        btn,
         validate: _validate,
         reset,
     };
