ea775cef8fa53a16ed0e77cc2e14b4817b2bc730
Kanimet <kanimet200127@gmail.com>
2024-08-10 15:29:57 +0600

    Backend: Configured cors for the server

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 import form from './routes/form.js';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
+
 app.use('/api/forms', form);
 
 app.listen(port, () => {
